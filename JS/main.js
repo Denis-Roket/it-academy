1a33c205432f5cdc51a164eac2d685da3543b649
Denis Rakeckiy <111denis2000@mail.ru>
2021-02-25 00:55:01 +0300

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('reviews-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      pagination: {
+        el: '.intro .swiper-pagination',
+        clickable: true,
+      },
+
+});
