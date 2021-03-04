044cfd0f52a1a1c8328245ac6541501a3f8e0c4a
Denis Rakeckiy <111denis2000@mail.ru>
2021-03-04 12:35:25 +0300

    update catalog-container

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 new Swiper ('.wr-reviews .swiper-container',{
+  slidesPerView: 2,
+  loop:true,
+  spaceBetween: 100,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
 });
+
+// tabs
+
