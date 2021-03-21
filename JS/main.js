3a1dffd3b53e8e122b83141a21da6b3cffdff6b8
Denis Rakeckiy <111denis2000@mail.ru>
2021-03-21 23:51:08 +0300

    final_commit

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,80 @@
-new Swiper ('.wr-reviews .swiper-container',{
-  slidesPerView: 2,
-  loop:true,
-  spaceBetween: 100,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-});
+if (document.getElementsByClassName('swiper-container').length) {
+  new Swiper('.wr-reviews .swiper-container', {
+    slidesPerView: 2,
+    loop: true,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  });
+}
 
 // tabs
 
+function openSweet(evt, sweetName) {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-item");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sweetName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+// // Get the element with id="defaultOpen" and click on it
+if (document.getElementById("defaultOpen")) {
+  document.getElementById("defaultOpen").click();
+}
+
+
+
+
+// top-button
+//Get the button:
+mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction(mybutton) };
+
+function scrollFunction(mybutton) {
+  if (mybutton) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "flex";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// tabs for product page
+
+function openInfo(evt, chekedText) {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-item");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(chekedText).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+// // Get the element with id="defaultOpen" and click on it
+if (document.getElementById("defaultOpen")) {
+  document.getElementById("defaultOpen").click();
+}
